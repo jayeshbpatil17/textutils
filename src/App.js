a853f370f501bbdb0f45f7172cbded34feb36868
Jayesh <jayeshbpatil17@gmail.com>
2023-06-09 02:06:45 +0530

    version2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+
+
 
 function App() {
   return (
     <>
+    
     <Navbar title="Patils" aboutText="About US"/>
     <div className="container">
-      
-    <TextForm head="Enter Text Below"/>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TextForm head="Enter Text Below"/>} />
+        <Route path="/about" element={<About />}>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+           
+      {/* <About/> */}
+      {/* <TextForm head="Enter Text Below"/> */}
     </div> 
+    
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,10 +21,7 @@ import PropTypes from 'prop-types'
           <a className="nav-link" href="/">{props.aboutText}</a>
         </li>
       </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+      
     </div>
   </div>
 </nav>
